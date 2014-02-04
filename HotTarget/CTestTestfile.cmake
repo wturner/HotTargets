1a441c8a6a02cdc907189702c94f6fa886b0185f
Brennon Brimhall <brennonbrimhall@gmail.com>
2014-02-03 20:38:22 -0500

    Restructured file system, added testing.

--- HotTarget/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jagen31/Robotics/HotTarget/HotTarget
-# Build directory: /home/jagen31/Robotics/HotTarget/HotTarget
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-ADD_TEST(Balls "/run" "/home/jagen31/Robotics/HotTarget/HotTarget/balls.jpg")
-SET_TESTS_PROPERTIES(Balls PROPERTIES  FAIL_REGULAR_EXPRESSION "blue ball finder found 0 match(es);red ball finder found 0 match(es)")
